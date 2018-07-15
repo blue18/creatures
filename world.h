e5a79e58d91f505a7be70bc18914d0d968a2d89c
blue18 <garcb969@icloud.com>
2018-07-14 19:04:37 -0700

    fixing issues with world class

--- world.h
+++ world.h
@@ -6,5 +6,16 @@ class World {
       World();
       ~World();
     private:
-
+      std::queue<Human> listOfHumans;
+      std::queue<Elf> listOfElves;
+      std::queue<Cyberdemon> listOfCyberdemon;
+      strd::queue<Balrog> listOfBalrogs;
+    public:
+      void addCreature();
+      void addCash();
+      void avaiableCash();
+      void enterNameOfCreature();
+      void startRound();
+      void startGame();
+      void printTheWinner();
 };
